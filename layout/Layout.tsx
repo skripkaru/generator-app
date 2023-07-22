a08e067e77bfdc1f08eb7fbfda27b83e47273d43
Evgeniy Skripkaru <skripkaru@ddplanet.ru>
2023-07-22 18:07:13 +0300

    update layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,10 +1,16 @@
-import {ReactNode, useState} from 'react'
+import React, {ReactNode, useState} from 'react'
 import Link from "next/link";
 import {Inter} from 'next/font/google'
 import styles from './Layout.module.scss'
 import cn from 'classnames'
-import {HomeOutlined, MenuFoldOutlined, MenuUnfoldOutlined} from "@ant-design/icons";
-import {Button} from "antd";
+import {
+  BarcodeOutlined,
+  NumberOutlined,
+  KeyOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined
+} from "@ant-design/icons";
+import {useRouter} from "next/router";
 
 const inter = Inter({subsets: ['latin', 'cyrillic']})
 
@@ -12,8 +18,32 @@ interface LayoutProps {
   children: ReactNode
 }
 
+const MenuItems = [
+  {
+    path: '/',
+    icon: <BarcodeOutlined style={{fontSize: 24}}/>,
+    label: 'Генератор артикулов'
+  },
+  {
+    path: '/password',
+    icon: <KeyOutlined style={{fontSize: 24}}/>,
+    label: 'Генератор паролей'
+  },
+  {
+    path: '/number',
+    icon: <NumberOutlined style={{fontSize: 24}}/>,
+    label: 'Генератор чисел'
+  }
+]
+
 const Layout = ({children}: LayoutProps) => {
-  const [active, setActive] = useState(false)
+  const [active, setActive] = useState<boolean>(false)
+  const router = useRouter();
+
+  const toggleActive = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault()
+    setActive(!active)
+  }
 
   return (
     <div className={cn(inter.className, styles.layout, {
@@ -22,27 +52,26 @@ const Layout = ({children}: LayoutProps) => {
       <aside className={styles.sidebar}>
         <nav className={styles.navbar}>
           <ul className={styles.list}>
-            <li>
-              <Button
+            <li className={styles.trigger}>
+              <Link
+                href='#'
                 className={styles.item}
-                type='text'
-                onClick={() => setActive(!active)}
+                onClick={toggleActive}
               >
-                {active ? <MenuFoldOutlined/> : <MenuUnfoldOutlined/>}
-                {active && 'Меню'}
-              </Button>
-            </li>
-            <li>
-              <Link href='/' className={styles.item}>
-                <Button
-                  className={styles.item}
-                  type='text'
-                >
-                  <HomeOutlined/>
-                  {active && 'Главная'}
-                </Button>
+                {active ? <MenuFoldOutlined style={{fontSize: 24}}/> : <MenuUnfoldOutlined style={{fontSize: 24}}/>}
+                {active && <span className={styles.label}>Меню</span>}
               </Link>
             </li>
+            {MenuItems.map(item =>
+              <li key={item.path}>
+                <Link href={item.path} className={cn(styles.item, {
+                  [styles.activeItem]: router.pathname === item.path
+                })}>
+                  {item.icon}
+                  {active && <span className={styles.label}>{item.label}</span>}
+                </Link>
+              </li>
+            )}
           </ul>
         </nav>
       </aside>
