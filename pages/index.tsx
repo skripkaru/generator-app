a984b8366a03df3f9b251465a187807d914c77ad
Evgeniy Skripkaru <skripkaru@ddplanet.ru>
2023-07-22 18:08:12 +0300

    update home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,80 +1,57 @@
-import React, {ChangeEvent, useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import Head from 'next/head'
 import Layout from "@/layout/Layout";
-import {Button, Checkbox, Col, Divider, Form, Input, Row} from "antd";
+import {Button, Col, Divider, Form, Input, InputNumber, Row} from "antd";
 
-export default function Password() {
+export default function Home() {
   const [code, setCode] = useState<string>('')
-  const [length, setLength] = useState<string>('8')
+  const [length, setLength] = useState<number>(8)
   const [symbols, setSymbols] = useState<string>('')
-  const [digits, setDigits] = useState<boolean>(false)
-  const [letters, setLetters] = useState<boolean>(false)
 
   useEffect(() => {
-    if (digits && letters) {
-      setSymbols('abcdefghijklmnopqrstuvwxyz0123456789')
-    } else if (digits) {
-      setSymbols('0123456789')
-    } else if (letters) {
-      setSymbols('abcdefghijklmnopqrstuvwxyz')
-    } else {
-      setSymbols('abcdefghijklmnopqrstuvwxyz0123456789')
-    }
-  }, [digits, letters, symbols])
+    setSymbols('0123456789')
+  }, [])
 
   const generateArticle = () => {
     let res = ''
 
-    for (let i = 0; i < Number(length); i++) {
+    for (let i = 0; i < length; i++) {
       res += symbols.charAt(Math.floor(Math.random() * symbols.length))
     }
 
     setCode(res)
   }
 
-  const changeLength = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value
-
-    if (isNaN(Number(value)) || value[0] === '-' || Number(value) > 24) {
-      return
-    }
-
-    setLength(value)
+  const changeLength = (value: number | null) => {
+    value && setLength(value)
   }
 
   return (
     <>
       <Head>
-        <title>Randomizer</title>
-        <meta name="description" content="Randomizer"/>
+        <title>Генератор артикулов</title>
+        <meta name="description" content="Генератор артикулов"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="icon" href="/favicon.ico"/>
       </Head>
 
       <Layout>
-        <h1>Генератор паролей</h1>
-        <Input style={{maxWidth: 320}} value={code}/>
+        <h1>Генератор артикулов</h1>
+        <Row>
+          <Col xs={24} md={12} lg={8}>
+            <Input size='large' value={code}/>
+          </Col>
+        </Row>
         <Divider/>
-
         <Form onFinish={generateArticle}>
-          <Row gutter={[16, 16]}>
+          <Row gutter={[16, 16]} align='middle'>
             <Col>
               <Form.Item label='Длина'>
-                <Input style={{maxWidth: 42}} value={length} onChange={changeLength}/>
-              </Form.Item>
-            </Col>
-            <Col>
-              <Form.Item label='Цифры'>
-                <Checkbox id='digits' checked={digits} onChange={() => setDigits(!digits)}/>
-              </Form.Item>
-            </Col>
-            <Col>
-              <Form.Item label='Строчная латиница'>
-                <Checkbox id='letters' checked={letters} onChange={() => setLetters(!letters)}/>
+                <InputNumber style={{maxWidth: 60}} size='large' min={1} max={24} defaultValue={length} onChange={changeLength}/>
               </Form.Item>
             </Col>
           </Row>
-          <Button type='primary' htmlType='submit'>
+          <Button type='primary' size='large' htmlType='submit'>
             Сгенерировать
           </Button>
         </Form>
